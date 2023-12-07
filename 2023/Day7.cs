3de0c74f3c52de5de72ec10213e5a45fab02af6c
JonaEnz <jona_enzinger@outlook.com>
2023-12-07 09:52:56 +0100

    2023 Day 7 Optimize

--- 2023/Day7.cs
+++ 2023/Day7.cs
@@ -25,7 +25,6 @@ public class Day07 : BaseDay
         ThreeOfKind = 4,
         TwoPair = 3,
         OnePair = 2,
-        High = 1,
         None = 0
     }
 
@@ -39,21 +38,21 @@ public class Day07 : BaseDay
         char ch => int.Parse(ch.ToString()),
     };
 
-    public static int SecondOrder(string hand, int joker)
-    {
-        return hand.Aggregate(0, (acc, c) => 15 * acc + CharStrength(c, joker));
-    }
+    public static int SecondOrder(string hand, int joker) => hand.Aggregate(0, (acc, c) => 15 * acc + CharStrength(c, joker));
 
     public static Hand CheckHand(string hand)
     {
-        if (hand.ToCharArray().Distinct().Count() == 1) return Hand.FiveOfKind;
-        var groupBy = hand.ToCharArray().GroupBy(x => x);
-        if (groupBy.Select(x => x.Count()).Max() == 4) return Hand.FourOfKind;
-        if (groupBy.Count(x => x.Count() == 2) == 1 && groupBy.Count(x => x.Count() == 3) == 1) return Hand.FullHouse;
-        if (groupBy.Select(x => x.Count()).Max() == 3) return Hand.ThreeOfKind;
-        if (groupBy.Where(x => x.Count() == 2).Count() == 2) return Hand.TwoPair;
-        if (hand.ToCharArray().GroupBy(x => x).Select(x => x.Count()).Max() == 2) return Hand.OnePair;
-        return Hand.None;
+        var sw = hand.ToCharArray().GroupBy(x => x).Select(x => x.Count()).OrderDescending().ToList();
+        return sw switch
+        {
+        [5, ..] => Hand.FiveOfKind,
+        [4, ..] => Hand.FourOfKind,
+        [3, 2, ..] => Hand.FullHouse,
+        [3, ..] => Hand.ThreeOfKind,
+        [2, 2, ..] => Hand.TwoPair,
+        [2, ..] => Hand.OnePair,
+            _ => Hand.None
+        };
     }
 
     public static Hand CheckHandPart2(string hand)
@@ -66,7 +65,6 @@ public class Day07 : BaseDay
         var maxCount = groups.Select(x => x.Count()).Max();
         var maxKind = groups.Where(x => x.Count() == maxCount).MaxBy(x => CharStrength(x.Key, 1)).Key;
         var hand2 = hand.Replace('J', maxKind);
-        Console.WriteLine($"{hand} -> {hand2}");
         return CheckHand(hand2);
     }
 
@@ -77,8 +75,7 @@ public class Day07 : BaseDay
             OrderBy(x => x.Key).
             Select(g => g.OrderBy(h => SecondOrder(h.Hand, 11))).
             SelectMany(x => x);
-        var r = ranking.Select((h, i) => h.Score * (i + 1)).Sum();
-        return new($"{r}");
+        return new($"{ranking.Select((h, i) => h.Score * (i + 1)).Sum()}");
     }
 
     public override ValueTask<string> Solve_2()
@@ -88,7 +85,6 @@ public class Day07 : BaseDay
             OrderBy(x => x.Key).
             Select(g => g.OrderBy(h => SecondOrder(h.Hand, 1))).
             SelectMany(x => x);
-        var r = ranking.Select((h, i) => h.Score * (i + 1)).Sum();
-        return new($"{r}");
+        return new($"{ranking.Select((h, i) => h.Score * (i + 1)).Sum()}");
     }
 }
